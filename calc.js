ad4ed49c555e7ef0017adc7e297b7c3706138fb5
SAIRAMMVK <39529800+SAIRAMMVK@users.noreply.github.com>
2018-06-01 05:50:29 +0530

    calc.js
    created a javascript file to handle the inputs , perform operations on it.

---
+++ calc.js
@@ -0,0 +1,23 @@
+document.getElementById("textval").onkeypress=function () {
+    var key = event.which;
+    if(key>=42 && key<58)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function calculate () {
+    var textvaluue = document.getElementById("textval").value;
+    //alert(textvaluue);
+    var answer = eval(textvaluue);
+    document.getElementById("textval").value=answer;
+}
+function emptyField () {
+    document.getElementById("textval").value="";
+}
+
+function addElement(inputElement) {
+    document.getElementById("textval").value += inputsElement;
+}
